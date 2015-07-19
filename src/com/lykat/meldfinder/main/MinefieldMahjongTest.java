8877157e04abd07ceed33ba45617d5cfa9e4dfa3
spen428 <spen428@users.noreply.github.com>
2015-07-19 17:41:27 +0100

    Renamed Test.java

--- src/com/lykat/meldfinder/main/MinefieldMahjongTest.java
+++ src/com/lykat/meldfinder/main/MinefieldMahjongTest.java
@@ -2,8 +2,31 @@ package com.lykat.meldfinder.main;
 
 import com.lykat.meldfinder.main.Hand.HandType;
 
-public class Test {
+/**
+ * A small program that I hacked together in order to attempt to find legal
+ * hands from within an arbitrary set of random mahjong tiles. It's nowhere near
+ * as efficient as it could be, but it does the job!
+ * 
+ * Used to verify the solution to the mathematical problem:
+ * 
+ * "What is the minimum number of *random* mahjong tiles required to guarantee
+ * that at least one legal hand of 14 tiles can be formed from the tiles?"
+ * 
+ * The answer - like the answer to the ultimate question of life, the universe
+ * and everything - appears to be 42 (136-94), which is immensely satisfying.
+ * 
+ * @author lykat
+ *
+ */
+public class MinefieldMahjongTest {
 
+	private static final int PASSES = 100000;
+	private static final int REMOVALS = 94;
+
+	public static void main(String args[]) {
+		new MinefieldMahjongTest().startTest(PASSES, REMOVALS);
+	}
+	
 	/**
 	 * Initiates a test run.
 	 * 
